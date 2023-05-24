104773ea02417eaa650d0c87b6377e5dd8cfa838
zlxm <zlxmmail@126.com>
2023-05-24 15:14:17 +0800

    dev_1.0

--- src/router/index.js
+++ src/router/index.js
@@ -21,17 +21,79 @@ export default new VueRouter({
         {
           path: "music",
           component: () => import("../views/UserMusic.vue"),
-        }
-      ]
+          children: [
+            {
+              path: "playlist/:id",
+              name: "Playlist",
+              component: () => import("../views/ListPage/PlaylistPage.vue"),
+              props: true,
+            },
+            {
+              path: "singerList/:id",
+              name: "Singerlist",
+              component: () => import("../views/ListPage/SingerListPage.vue"),
+              props: true,
+            },
+          ],
+        },
+      ],
     },
     {
       path: "/homepage",
+      redirect: "/homepage/discover",
       component: () => import("../views/HomePage.vue"),
+      children: [
+        {
+          path: "discover",
+          component: () => import("../views/homepage/DiscoverPage.vue"),
+        },
+        {
+          path: "toplist",
+          component: () => import("../views/homepage/TopListPage.vue"),
+        },
+        {
+          path: "playlist",
+          component: () => import("../views/homepage/PlayListPage.vue"),
+        },
+        {
+          path: "artist",
+          component: () => import("../views/homepage/ArtistPage.vue"),
+        },
+        {
+          path: "song",
+          component: () => import("../views/homepage/SongPage.vue"),
+        },
+      ],
+    },
+    {
+      path: "/album/:id",
+      name: "AlbumPage",
+      component: () => import("../views/AlbumPage.vue"),
+      props: true,
+    },
+    {
+      path: "/singer/:id",
+      name: "SingerPage",
+      component: () => import("../views/SingerPage.vue"),
+      props: true,
+    },
+    {
+      path: "/song/:id",
+      name: "SongPage",
+      component: () => import("../views/SongPage.vue"),
+      props: true,
+    },
+    {
+      path: "/search",
+      component: () => import("../views/Search.vue"),
     },
     {
       path: "/social",
       component: () => import("../views/SocialPage.vue"),
     },
-    }
+    {
+      path: "/creator",
+      component: () => import("../views/Creator.vue"),
+    },
   ],
 });
