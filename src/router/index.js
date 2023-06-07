7f058cf2c67c56596835ce58817fe9e5b0ed484a
shyJyt <2873704761@qq.com>
2023-06-07 21:26:25 +0800

    'fix-router'

--- src/router/index.js
+++ src/router/index.js
@@ -4,179 +4,156 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    routes: [{
-            path: "/",
-            redirect: "/homepage",
+  routes: [
+    {
+      path: "/",
+      redirect: "/homepage",
+    },
+    {
+      path: "/user",
+      redirect: "/user/home",
+      component: () => import("../views/UserPage.vue"),
+      children: [
+        {
+          path: "home",
+          component: () => import("../views/UserHome.vue"),
         },
         {
-            path: "/user",
-            redirect: "/user/home",
-            component: () =>
-                import ("../views/UserPage.vue"),
-            children: [{
-                    path: "home",
-                    component: () =>
-                        import ("../views/UserHome.vue"),
-                },
-                {
-                    path: "music",
-                    component: () =>
-                        import ("../views/UserMusic.vue"),
-                    children: [{
-                            path: "musiclist/:id",
-                            name: "Musiclist",
-                            component: () =>
-                                import ("../views/ListPage/MusiclistPage.vue"),
-                            props: true,
-                        },
-                        {
-                            path: "singerList/:id",
-                            name: "Singerlist",
-                            component: () =>
-                                import ("../views/ListPage/SingerListPage.vue"),
-                            props: true,
-                        },
-                        {
-                            path: "myupload",
-                            name: "MyUpload",
-                            component: () =>
-                                import ("../views/ListPage/MySongPage.vue"),
-                        },
-                    ],
-                },
-                {
-                    path: "setting",
-                    component: () =>
-                        import ("../views/UserSetting.vue"),
-                },
-                {
-                    path: "message",
-                    component: () =>
-                        import ("../views/UserMessage.vue"),
-                    // redirect: "/user/message/comment",
-                    children: [{
-                            path: "comment",
-                            component: () =>
-                                import ("../views/Message/MyCommentPage.vue"),
-                        },
-                        {
-                            path: "reply",
-                            component: () =>
-                                import ("../views/Message/ReplyToMe.vue")
-                        },
-                        {
-                            path: "complaint",
-                            component: () =>
-                                import ("../views/Message/ComplaintPage.vue"),
-                        },
-                        {
-                            path: "complaint-detail",
-                            component: () =>
-                                import ("../views/Message/ComplaintDetailPage.vue"),
-                        },
-                        {
-                            path: "notice",
-                            component: () =>
-                                import ("../views/Message/NoticePage.vue"),
-                        },
-                        {
-                            path: "exam",
-                            component: () =>
-                                import ("../views/Message/ExamPage.vue"),
-                        },
-                        {
-                            path: "exam-detail",
-                            component: () =>
-                                import ("../views/Message/ExamDetailPage.vue"),
-                        },
-                    ],
-                },
-            ],
+          path: "music",
+          component: () => import("../views/UserMusic.vue"),
+          children: [
+            {
+              path: "musiclist/:id",
+              name: "Musiclist",
+              component: () => import("../views/ListPage/MusiclistPage.vue"),
+              props: true,
+            },
+            {
+              path: "singerList/:id",
+              name: "Singerlist",
+              component: () => import("../views/ListPage/SingerListPage.vue"),
+              props: true,
+            },
+            {
+              path: "myupload",
+              name: "MyUpload",
+              component: () => import("../views/ListPage/MySongPage.vue"),
+            },
+          ],
         },
         {
-            path: "/homepage",
-            redirect: "/homepage/discover",
-            component: () =>
-                import ("../views/HomePage.vue"),
-            children: [{
-                    path: "discover",
-                    component: () =>
-                        import ("../views/homepage/DiscoverPage.vue"),
-                },
-                {
-                    path: "toplist",
-                    component: () =>
-                        import ("../views/homepage/TopListPage.vue"),
-                },
-                {
-                    path: "playlist",
-                    component: () =>
-                        import ("../views/homepage/PlayListPage.vue"),
-                    props: true,
-                },
-                {
-                    path: "artist",
-                    component: () =>
-                        import ("../views/homepage/ArtistPage.vue"),
-                },
-                {
-                    path: "song",
-                    component: () =>
-                        import ("../views/homepage/SongPage.vue"),
-                },
-            ],
+          path: "setting",
+          component: () => import("../views/UserSetting.vue"),
         },
         {
-            path: "/playlist/:id",
-            name: "PlaylistPage",
-            component: () =>
-                import ("../views/PlaylistPage.vue"),
-            props: true,
-        },
-        {
-            path: "/singer/:id",
-            name: "SingerPage",
-            component: () =>
-                import ("../views/SingerPage.vue"),
-            props: true,
-        },
-        {
-            path: "/song/:music_id",
-            name: "SongPage",
-            component: () =>
-                import ("../views/SongPage.vue"),
-            props: true,
-        },
-        {
-            path: "/playing/:music_id",
-            name: "PlayingSongPage",
-            component: () =>
-                import ("../views/PlayingSongPage.vue"),
-            props: true,
-        },
-        {
-            path: "/search",
-            component: () =>
-                import ("../views/Search.vue"),
-        },
-        {
-            path: "/social",
-            component: () =>
-                import ("../views/SocialPage.vue"),
-            children: [{
-                    path: "follow",
-                    component: () =>
-                        import ("../views/Social/FollowPage.vue"),
-                },
-                {
-                    path: "fan",
-                    component: () =>
-                        import ("../views/Social/FanPage.vue"),
-                },
-            ],
+          path: "message",
+          component: () => import("../views/UserMessage.vue"),
+          // redirect: "/user/message/comment",
+          children: [
+            {
+              path: "comment",
+              component: () => import("../views/Message/MyCommentPage.vue"),
+            },
+            {
+              path: "reply",
+              component: () => import("../views/Message/ReplyToMe.vue"),
+            },
+            {
+              path: "complaint",
+              component: () => import("../views/Message/ComplaintPage.vue"),
+            },
+            {
+              path: "complaint-detail",
+              component: () =>
+                import("../views/Message/ComplaintDetailPage.vue"),
+            },
+            {
+              path: "notice",
+              component: () => import("../views/Message/NoticePage.vue"),
+            },
+            {
+              path: "exam",
+              component: () => import("../views/Message/ExamPage.vue"),
+            },
+            {
+              path: "exam-detail",
+              component: () => import("../views/Message/ExamDetailPage.vue"),
+            },
+          ],
         },
       ],
     },
+    {
+      path: "/homepage",
+      redirect: "/homepage/discover",
+      component: () => import("../views/HomePage.vue"),
+      children: [
+        {
+          path: "discover",
+          component: () => import("../views/homepage/DiscoverPage.vue"),
+        },
+        {
+          path: "toplist",
+          component: () => import("../views/homepage/TopListPage.vue"),
+        },
+        {
+          path: "playlist",
+          component: () => import("../views/homepage/PlayListPage.vue"),
+          props: true,
+        },
+        {
+          path: "artist",
+          component: () => import("../views/homepage/ArtistPage.vue"),
+        },
+        {
+          path: "song",
+          component: () => import("../views/homepage/SongPage.vue"),
+        },
+      ],
+    },
+    {
+      path: "/playlist/:id",
+      name: "PlaylistPage",
+      component: () => import("../views/PlaylistPage.vue"),
+      props: true,
+    },
+    {
+      path: "/singer/:id",
+      name: "SingerPage",
+      component: () => import("../views/SingerPage.vue"),
+      props: true,
+    },
+    {
+      path: "/song/:music_id",
+      name: "SongPage",
+      component: () => import("../views/SongPage.vue"),
+      props: true,
+    },
+    {
+      path: "/playing/:music_id",
+      name: "PlayingSongPage",
+      component: () => import("../views/PlayingSongPage.vue"),
+      props: true,
+    },
+    {
+      path: "/search",
+      component: () => import("../views/Search.vue"),
+    },
+    {
+      path: "/social",
+      component: () => import("../views/SocialPage.vue"),
+      children: [
+        {
+          path: "follow",
+          component: () => import("../views/Social/FollowPage.vue"),
+        },
+        {
+          path: "fan",
+          component: () => import("../views/Social/FanPage.vue"),
+        },
+      ],
+    },
+
     {
       path: "/creator",
       component: () => import("../views/Creator.vue"),
@@ -209,7 +186,7 @@ const router = new VueRouter({
       name: "Dev",
       props: true,
       component: () => import("../views/dev.vue"),
-    }
+    },
   ],
 });
 
